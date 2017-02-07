6369ea1ec2c1875be3b64b79fbce7b094090493c
Sam Eastham <sameastham@gmail.com>
2017-02-07 13:11:20 -0500

    initial commit

---
+++ customJS.js
@@ -0,0 +1,14 @@
+$('body').scrollspy({
+  target: '#navbar',
+  offset: 52
+});
+
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
